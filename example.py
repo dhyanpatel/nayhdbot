6f312d8d1822a93c2571cf45b89137add354df19
Dhyan Patel <dhyanpp@gmail.com>
2017-10-27 13:24:07 -0500

    command example

--- example.py
+++ example.py
@@ -3,3 +3,15 @@ from discord.ext import commands
 Bot = commands.Bot(command_prefix='+', description='<Bot Name>")
 
 # This creates the Bot! With this you can do the commands extension.
+
+                   
+#This is an example commmand to see 
+@bot.command(pass_context = True, aliases = ['Ping'])
+async def ping(ctx):
+    """Pings the Discord servers and returns the response time."""
+    await bot.send_typing(ctx.message.channel)
+    startTime = time.monotonic()
+    await (await bot.ws.ping())
+    endTime = time.monotonic()
+    ping = (endTime - startTime) * 1000
+    await bot.say('The server response time was {}ms'.format(int(ping)))
